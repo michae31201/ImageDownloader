63544abf6831612d2c30c970fd986c21fb669e51
michae31201 <michae31201@gmail.com>
2020-04-03 18:49:11 +0800

    change fetch methods to 10 requests per batch

--- src/utils/getBlobUrl.js
+++ src/utils/getBlobUrl.js
@@ -1,8 +1,8 @@
 async function getBlobUrl(urls){
   //const crosUrl = 'https://cors-anywhere.herokuapp.com/';
   //let requests = urls.map((url)=> fetch(`${crosUrl}${url}`));
-  let requests = urls.map((url)=> fetch(`/.netlify/functions/fetchImage?site=${url}`));
-  let responses = await Promise.all(requests);
+  let requests = getFetchArray(urls)
+  let responses = await getResponseArray(requests);
   let parseBlob = responses.map((response) => (response.blob()));
   let results = await Promise.all(parseBlob);
   //使用 URL.createObjectURL 將 blob 轉為同源的圖片網址
@@ -12,5 +12,23 @@ async function getBlobUrl(urls){
   })
   return files;
 } 
+
+function getFetchArray(urls){
+  let result = [];
+  while(urls.length){
+    let tmp = urls.splice(0,10);
+    result.push(tmp.map((url)=> fetch(`/.netlify/functions/fetchImage?site=${url}`)));
+  }
+  return result;
+}
+async function getResponseArray(requests){
+  let result = [];
+  for(let req of requests){
+    result = result.concat(await Promise.all(req));
+  }
+  return result;
+
+
+}
 
 export default getBlobUrl;
